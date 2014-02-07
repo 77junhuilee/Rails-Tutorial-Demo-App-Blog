e2ed1783eb4380706fd973d03d743cc5005d7a1c
yyjunhuilee <lijh@yonyou.com>
2014-02-07 16:08:16 +0800

    Make a basic User Model including secrure passwords(功能 #2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base	
-  	attr_accessible :email, :name
+  	attr_accessible :email, :name, :password, :password_confirmation
   	has_many :microposts
+  	has_secure_password
+
+  	before_save { self.email = email.downcase }
+  	
+  	validates :name,  presence: true, length: { maximum: 50 }
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, 
+  						format: {with: VALID_EMAIL_REGEX},
+  						uniqueness: {case_sensitive: false }
+  	validates :password, length: { minimum: 6 }
 end
