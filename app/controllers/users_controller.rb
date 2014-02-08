7fcb69ff9d7dc869dde416efd14c039097183d44
yyjunhuilee <lijh@yonyou.com>
2014-02-08 11:58:44 +0800

    finished sign up(Fun. #5)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to the Sample App!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
@@ -52,6 +53,12 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  private 
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 
   # PUT /users/1
   # PUT /users/1.json
